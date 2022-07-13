0b5ddd809d0d6759643bc6b1b771016c7472b7a0
chaman21 <76278258+chaman21@users.noreply.github.com>
2022-07-13 12:41:04 +0530

    Update app.js
    Signed-off-by: chaman21 <76278258+chaman21@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-function addItem(){
-    let ul = document.getElementById("list");
-    let item = document.getElementById("item");
-    let li = document.createElement("li");
-    li.setAttribute('id',item.value);
-    li.appendChild(document.createTextNode(item.value));
-    ul.appendChild(li);
-}
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
 
-function removeItem(){
-    let ul = document.getElementById("list");
-    let item = document.getElementById("item");
-    let remove = document.getElementById(item.value);
-    ul.removeChild(remove);
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+</svg>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
 }
